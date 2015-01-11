b5ac191169ea83638358fc191c958ff6a0de6eea
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-01-11 12:44:43 +0100

    added gruntfile for test in future

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+       
+    });
+
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  
+   
+};
