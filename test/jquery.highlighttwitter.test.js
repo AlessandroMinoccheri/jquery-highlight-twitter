c8e681355c270b53ad38065dfc584c3864db7825
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2015-01-14 21:39:31 +0100

    implement gruntfile command to test and files to test in different jquery versions

--- test/jquery.highlighttwitter.test.js
+++ test/jquery.highlighttwitter.test.js
@@ -1,10 +1,11 @@
 var QUNIT = true;
 $(document).ready(function(){
   var testfield = $('.txt')
-  
+
   module('Simple Hihglight');
     test("Text to highlight", function(){
-      
-
+      testfield.focus();
+      testfield.selectionStart = 10;
+      testfield.selectionEnd = 100;
     });
 });
